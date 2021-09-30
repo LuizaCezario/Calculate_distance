fb5c60e54f31d509c12f0290ecfdf82ea706f908
LuizaCezario <you@example.com>
2021-09-30 12:22:47 -0300

    Changing comment.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from blueprint import calculate_route
 
 app = Flask(__name__)
-#here is registrated the route to access the page 
+# here is registrated the route to access the page 
 app.register_blueprint(calculate_route, url_prefix='/')
 app.register_blueprint(calculate_route, url_prefix='/api')
 
